9d2dafbffe3c07f8b4c5397e04694a0b00b75959
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-02-05 14:13:01 +0100

    Job 1 Fonction connect

--- job1/user.php
+++ job1/user.php
@@ -1,5 +1,7 @@
 <?php
 	
+	session_start();
+
 	class user 
 	{
 		private $id =" ";
@@ -29,13 +31,40 @@
 
 		}
 
+		public function connect($login,$password)
+		{
+			$connexion = mysqli_connect("localhost","root","","poo_test");
+			$selectUser = "SELECT * FROM user WHERE login ='$login'";
+			$querySelect = mysqli_query($connexion,$selectUser);
+			$resultSelect = mysqli_fetch_assoc($querySelect);
+
+			if ($password == $resultSelect['password']) 
+			{
+				$_SESSION['login'] = $login ;
+				$_SESSION['password]'] = $password ;
+
+				$this->login = $resultSelect['login'] ;
+				$this->password = $resultSelect['password'] ;
+				$this->email = $resultSelect['email'] ;
+				$this->firstname = $resultSelect['firstname'] ;
+				$this->lastname = $resultSelect['lastname'] ;
+
+				echo "T CO FREROT \o/ ";
+			}
+			else
+			{
+				echo "T PAS CO MON POTE" ;
+			}
+		}
+
 		
 	}
 
 	$user = new user();
 
-	$user->register('Katakuri','mohamed.azzouz@laplateforme.io','Azzouz','Mohamed','Kata') ;
+	#$user->register('Katakuri','mohamed.azzouz@laplateforme.io','Azzouz','Mohamed','Kata') ;
 
+	$user->connect('Katakuri','Kata') ;
 	
 
 ?>
