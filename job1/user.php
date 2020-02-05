d93677482ff6d81b65c46b47640defaedf48102c
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-02-05 17:00:05 +0100

    Ajout Fonction disconnect(), delete(), update(), isConnected().

--- job1/user.php
+++ job1/user.php
@@ -1,6 +1,6 @@
 <?php
 	
-	session_start();
+	
 
 	class user 
 	{
@@ -26,7 +26,7 @@
 			$resultSelect = mysqli_fetch_all($querySelect);
 
 
-			var_dump($resultSelect) ;
+			
 			return $resultSelect ;
 
 		}
@@ -40,31 +40,88 @@
 
 			if ($password == $resultSelect['password']) 
 			{
-				$_SESSION['login'] = $login ;
-				$_SESSION['password]'] = $password ;
 
+				
 				$this->login = $resultSelect['login'] ;
 				$this->password = $resultSelect['password'] ;
 				$this->email = $resultSelect['email'] ;
 				$this->firstname = $resultSelect['firstname'] ;
 				$this->lastname = $resultSelect['lastname'] ;
+				$_SESSION['momo'] = $this;
 
-				echo "T CO FREROT \o/ ";
+				echo "Vous etes co<br />";
 			}
 			else
 			{
-				echo "T PAS CO MON POTE" ;
+				echo "Mauvais log <br />" ;
+			}
+		}
+
+		public function disconnect()
+		{
+			session_unset();
+			session_destroy();
+
+			$this->login = null;
+			$this->password = null;
+			$this->email = null;
+			$this->firstname = null;
+			$this->lastname = null;
+
+			echo "Vous etes deco <br />";
+		}
+
+		public function delete()
+		{
+			$connexion = mysqli_connect("localhost","root","","poo_test");
+			$requeteDelete = "DELETE FROM user WHERE login = '$this->login'";
+			$queryDelete = mysqli_query($connexion,$requeteDelete) ;
+
+			$this->disconnect();
+			echo "ON TA SUPPRIME <br />";
+		}
+
+		public function update($login,$email,$firstname,$lastname,$password)
+		{
+			$connexion = mysqli_connect("localhost","root","","poo_test");
+			$update = "UPDATE user SET login = '$login', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE login =".$_SESSION['login']." " ;
+			$queryUpdate = mysqli_query($connexion,$update) ;
+
+			echo "MODIF FAITE";
+		}
+
+		public function isConnected()
+		{
+			if (isset($_SESSION['momo'])) 
+			{
+				return true ;
+
+			}
+			else
+			{
+				return false;
+
 			}
 		}
 
 		
 	}
 
+	session_start();
+
 	$user = new user();
 
-	#$user->register('Katakuri','mohamed.azzouz@laplateforme.io','Azzouz','Mohamed','Kata') ;
+	$user->register('PAUL','mohamed.azzouz@laplateforme.io','SERGE','POMMM','TITI') ;
 
-	$user->connect('Katakuri','Kata') ;
-	
+	$user->connect('PAUL','TITI') ;
+
+	$user->disconnect();
+
+	#$user->delete() ;
+
+	#$user->update('Luffy','luffy.Katakuri@laplateforme.io','MonkeyD','Omaewa','LOP') ;
+
+	$test = $user->isConnected();
+	var_dump($test);	
 
 ?>
